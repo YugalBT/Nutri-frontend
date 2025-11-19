0eef67d752fafff4ec754559f2ff40da4327fd6e
yugal <yugal@braintechnosys.com>
2025-11-19 14:18:17 +0530

    content chage of mian and fix header and sidebar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DashboardComponent } from '../features/dashboard/dashboard.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [DashboardComponent,SidebarComponent,RouterOutlet],
+  imports: [DashboardComponent,SidebarComponent,HeaderComponent,RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
