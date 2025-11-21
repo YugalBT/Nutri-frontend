9a135709bf0e6ebb5cedc3d5de4f0ed544a06a55
anurag <anurag@braintechnosys.com>
2025-11-21 17:53:38 +0530

    changes in common module

--- src/app/shared/components/common-modal/common-modal.component.ts
+++ src/app/shared/components/common-modal/common-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TranslatePipe } from '../../../i18n/translate.pipe';
+import { SharedModule } from '../../shared.module';
 
 @Component({
   selector: 'app-common-modal',
   standalone: true,
-  imports: [TranslatePipe],
+  imports: [TranslatePipe,SharedModule],
   templateUrl: './common-modal.component.html',
   styleUrls: ['./common-modal.component.css']
 })
