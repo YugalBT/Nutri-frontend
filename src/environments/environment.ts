0a2bc7e9ef605245169e841eda9a732b80f7f08c
Gaurav0Yadav <gaurav@braintechnosys.com>
2025-12-18 15:37:40 +0530

    Env fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 const environment = {
     production: false,
-    //apiUrl: 'https://nutri.btprojects.in/apis/api'
-    apiUrl: 'https://localhost:7206/api'
+    apiUrl: 'https://nutri.btprojects.in/apis/api'
+    //apiUrl: 'https://localhost:7206/api'
 }
